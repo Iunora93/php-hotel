98f6d83297addfc48e50d717183cef3a79cd06f7
Iunora93 <eleonoraruggieri22@gmail.com>
2022-12-14 15:49:47 +0100

    Aggiunge table Bootstrap

--- index.php
+++ index.php
@@ -51,9 +51,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Hotel</title>
+    <!-- Bootstrap-link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <!-- /Bootstrap-link -->
 </head>
 <body>
-    <ul>
+    <ul class="container-md">
         <?php foreach ($hotels as $key => $hotel) { ?>
             <h3><?php echo $hotel['name']; ?></h3>
             <li><?php echo $hotel['description']; ?></li>
@@ -62,6 +67,24 @@
             <li><?php echo $hotel['distance_to_center']; ?></li>
         <?php } ?>
     </ul>
+
+    <table class="table">
+        <?php foreach ($hotels as $key => $hotel) { ?>
+            <thead>           
+                <tr>               
+                    <th scope="col"><?php echo $hotel['name']; ?></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>           
+                    <td><?php echo $hotel['description']; ?></td>
+                    <td><?php echo $hotel['parking']; ?></td>
+                    <td><?php echo $hotel['vote']; ?></td>
+                    <td><?php echo $hotel['distance_to_center']; ?></td>
+                </tr>           
+            </tbody>
+        <?php } ?>
+    </table>
 </body>
 </html>
 
